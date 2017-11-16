1731a5cb8952bbf47805f34f9e8a3e37a42791f3
Максим Марченко <slayer.jk@mail.ru>
2017-11-16 16:30:51 +0600

    Html done.

---
+++ app/test.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <title>Photobook - free psd</title>
+    
+  <link rel="stylesheet" href="libs/bootstrap.min.css">
+  <link href="css/style.min.css" rel="stylesheet">
+
+  <link rel="icon" href="img/icons/favicon_32x32.ico">
+  <link rel="icon" href="img/icons/favicon_16x16.ico">
+
+  <!--[if lt IE 9]>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+
+  <header class="page-header">
+    <div class="page-header__wrapper wrapper-global">
+      <nav class="main-nav">
+				<h1 class="test">Title!_for_PHP_qfgffTest_PAGE234</h1>
+        <a href="test.php">Index</a>
+      </nav>
+    </div>
+  </header>
+
+  <footer class="page-footer">
+    <div class="page-footer__wrapper wrapper-global">
+
+    </div>
+  </footer>
+
+  <!--
+  <div class="overlay"></div>
+  <div class="modal-windows"></div>
+  -->
+
+  <!-- Scripts______________________________________ -->
+  <script src="libs/jquery-3.2.1.min.js"></script>
+	<script src="libs/bootstrap.min.js"></script>
+  <!--<script src="js/picturefill.min.js"></script>-->
+
+  <!--<script src="js/script.min.js"></script>-->
+  <script src="js/script.js"></script>
+
+</body>
+
+</html>
